7db4d02d30895562f6517cb79221e779a8abad2d
dada123-pbE4z <741306298@qq.com>
2025-12-11 15:27:21 +0800

    111

--- brt-admin/src/main/java/com/brt/order/controller/BrtScriptController.java
+++ brt-admin/src/main/java/com/brt/order/controller/BrtScriptController.java
@@ -1,14 +1,11 @@
 package com.brt.order.controller;
 
 import com.brt.common.core.domain.AjaxResult;
+import com.brt.productionflow.vo.ProdApiQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RestController
 @RequestMapping("/script/api")
@@ -22,6 +19,24 @@ public class BrtScriptController {
 
     @PostMapping("/second")
     public AjaxResult second(){
+        return AjaxResult.success();
+    }
+
+    @PostMapping("/flowFirst")
+    public AjaxResult flowFirst(@RequestBody ProdApiQuery param){
+        System.out.println(param);
+        return AjaxResult.success();
+    }
+
+    @PostMapping("/flowSecond")
+    public AjaxResult flowSecond(@RequestBody ProdApiQuery param){
+        System.out.println(param);
+        return AjaxResult.success();
+    }
+
+    @PostMapping("/flowThird")
+    public AjaxResult flowThird(@RequestBody ProdApiQuery param){
+        System.out.println(param);
         return AjaxResult.success();
     }
 
---
+++ brt-system/src/main/java/com/brt/productionflow/vo/ProdApiQuery.java
@@ -0,0 +1,32 @@
+package com.brt.productionflow.vo;
+
+public class ProdApiQuery {
+
+    private String orderId;
+    private String nodeId;
+    private String flowId;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public String getFlowId() {
+        return flowId;
+    }
+
+    public void setFlowId(String flowId) {
+        this.flowId = flowId;
+    }
+}
